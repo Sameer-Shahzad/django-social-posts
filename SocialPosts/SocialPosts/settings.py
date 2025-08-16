122008d245238ada43f5b348ab364229af31e07e
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-08-17 01:03:18 +0500

    second commit

--- SocialPosts/SocialPosts/settings.py
+++ SocialPosts/SocialPosts/settings.py
@@ -56,10 +56,11 @@ ROOT_URLCONF = 'SocialPosts.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR/ 'templates')],
+        'DIRS': [BASE_DIR / 'SocialPosts' / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -67,6 +68,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'SocialPosts.wsgi.application'
 
