34b6f25dc7fb2aaf77b4ad5185b7a085bd0f175d
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-08-19 23:54:22 +0500

    fourth commit

--- SocialPosts/tweet/views.py
+++ SocialPosts/tweet/views.py
@@ -4,15 +4,16 @@ from .forms import TweetForm
 from django.shortcuts import get_object_or_404, redirect
 # Create your views here.
 
+from django.contrib.auth.decorators import login_required
+
 def index(request):
     return render (request, 'index.html')
-
 
 def tweet_list (request):
     tweets = Tweet.objects.all().order_by('-created_at')
     return render (request, 'tweet_list.html', {'tweets': tweets})
 
-
+@login_required
 def tweet_create(request):
     if request.method == "POST":
         form = TweetForm(request.POST, request.FILES)
@@ -25,7 +26,7 @@ def tweet_create(request):
         form = TweetForm()
     return render (request, 'tweet_form.html', {'form': form})
 
-
+@login_required
 def tweet_edit(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user = request.user)
     if request.method == "POST":
@@ -39,6 +40,7 @@ def tweet_edit(request, tweet_id):
         form = TweetForm(instance=tweet)
     return render(request, 'tweet_form.html', {'form': form})
 
+@login_required
 def tweet_delete(request, tweet_id):
     tweet = get_object_or_404(Tweet, pk=tweet_id, user=request.user)
     if request.method == "POST":
