c91175b3e14726c210edc23fd6fc6e57f2d61fa6
Sameer-Shahzad <sameershahzad835@gmail.com>
2025-08-22 17:44:30 +0500

    sixth commit

--- SocialPosts/tweet/models.py
+++ SocialPosts/tweet/models.py
@@ -8,7 +8,8 @@ from django.contrib.auth.models import User
 class Tweet (models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     text = models.TextField(max_length= 240)
-    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    photo = models.ImageField(upload_to="photos/", blank=True, null=True)
+    # photo = models.ImageField(upload_to="photos/", blank=True, null=True, default="default.png")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     
